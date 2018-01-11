9d36e779c57fb88edab950e9cf2a13728cf73886
Donatien Rolland <donatien@rollandmail.com>
2018-01-11 14:57:28 +0100

    tab and add done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,5 @@
-// <div className={divClasses} onClick={this.handleClick}>
-
 // = require rails-ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
-// [...]
 
-handleClick = () => {
-  Rails.ajax({
-    url: `/pictures/${this.props.picture.id}/upvote`,
-    type: 'POST',
-    success: (data) => {
-      this.setState({ picture: data })
-    }
-  });
-}
--- app/javascript/components/camera.js
+++ app/javascript/components/camera.js
@@ -1,7 +1,12 @@
-const addphoto = document.getElementById("picture_photo");
+const addphoto = document.getElementById("add-photo");
 // addphoto.type = "file";
 
 document.getElementById("camera").addEventListener("click", function(){
   console.log('titi')
   addphoto.click();
 });
+
+document.getElementById("add-photo").onchange = function (){
+  document.getElementById('myForm').submit();
+};
+
