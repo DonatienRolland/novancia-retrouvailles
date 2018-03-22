50e24f49bb86c67e7f504c0df0d6fa3aa632237b
Donatien Rolland <donatien@rollandmail.com>
2018-03-22 11:16:03 +0100

    change url

--- app/javascript/components/fetchPicture.js
+++ app/javascript/components/fetchPicture.js
@@ -1,7 +1,7 @@
 function RefreshLeft() {
 
   const photoLeft = document.getElementById("photoRightPart");
-  var url = 'http://localhost:3000/last-picture'
+  var url = 'https://novancia-retrouvailles.herokuapp.com/last-picture'
 
   fetch(url, {
     credentials: 'same-origin'
